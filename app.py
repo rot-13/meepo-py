db4d802f414810308c19ef5c7a734e8fec04c655
Elad Shaham <eshaham@paypal.com>
2017-07-02 15:46:14 +0300

    fix app

--- app.py
+++ app.py
@@ -7,20 +7,22 @@ WHITE = (255,255,255)
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
-pygame.mouse.set_visible(False)
 lcd = pygame.display.set_mode((320, 240))
+pygame.mouse.set_visible(False)
 lcd.fill((0,0,0))
 pygame.display.update()
 
 font_big = pygame.font.Font(None, 100)
 
 running = True
+k = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     k += 1
+    lcd.fill((0,0,0))
     text_surface = font_big.render('%d'%k, True, WHITE)
     rect = text_surface.get_rect(center=(160,120))
     lcd.blit(text_surface, rect)
