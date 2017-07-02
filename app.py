afbb20f3f2bd4bbbbd4e9b5ca44c17d95d3e00bc
Elad Shaham <eshaham@paypal.com>
2017-07-02 16:37:41 +0300

    show list of people

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import pygame
 import os
 from time import sleep
+import requests
 
 #Colours
 WHITE = (255,255,255)
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
-lcd = pygame.display.set_mode((320, 240))
+lcd = pygame.display.set_mode((320, 240), pygame.FULLSCREEN)
 pygame.mouse.set_visible(False)
 lcd.fill((0,0,0))
 pygame.display.update()
 
-font_big = pygame.font.Font(None, 100)
+font_medium = pygame.font.Font(None, 30)
 
 running = True
 k = 0
@@ -21,9 +22,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    r = requests.get("https://meepo-api.herokuapp.com/people")
+    people_str = "";
+    for person in r.json()['people']:
+        people_str += person['name'] + ', '
+
     k += 1
     lcd.fill((0,0,0))
-    text_surface = font_big.render('%d'%k, True, WHITE)
+    text_surface = font_medium.render(people_str, True, WHITE)
     rect = text_surface.get_rect(center=(160,120))
     lcd.blit(text_surface, rect)
     pygame.display.update()
